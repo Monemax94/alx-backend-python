dca85e1b3eb6eb78f4980d16ebf29566fbaaef87
Monemax94 <mondayodoh94gmail.com>
2023-04-03 19:36:24 +0100

    tasks completion on asyncio

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,7 +1,6 @@
-
 #!/usr/bin/env python3
-'''Task 4's module.
-'''
+"""Task 4's module.
+"""
 import asyncio
 from typing import List
 
@@ -10,8 +9,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Executes task_wait_random n times.
-    '''
+    """Executes task_wait_random n times.
+    """
     wait_times = await asyncio.gather(
         *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
     )
